b264e4555053973cbda0b8e45eba764d78073211
Chayaphon256 <pium.vipant@gmail.com>
2022-03-04 17:14:57 +0700

    first commit

---
+++ Assets/ChangeAnimators.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getCapoeira(){
+        this.GetComponent<Animator>().Play("Capoeira");
+    }
+    public void getDancingTwerk(){
+        this.GetComponent<Animator>().Play("Dancing Twerk");
+    }
+}
